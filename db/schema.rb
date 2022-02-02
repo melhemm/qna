d3a82353e387160556cb290b54d71940f6631619
melhemm <jamalmulhemm@gmail.com>
2022-02-02 23:54:00 +0300

    add_question_id_to_answers migration removed

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_02_195721) do
+ActiveRecord::Schema.define(version: 2022_02_02_194643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
-    t.text "body"
+    t.text "body", null: false
+    t.bigint "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "question_id"
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
@@ -30,4 +30,5 @@ ActiveRecord::Schema.define(version: 2022_02_02_195721) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "answers", "questions"
 end
