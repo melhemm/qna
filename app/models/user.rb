dd05abe55770dd989b4e7a49b3e38d8a0bffc7c4
melhemm <jamalmulhemm@gmail.com>
2022-02-09 23:55:28 +0300

    user model with association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :questions, dependent: :destroy, foreign_key: :user_id
+  has_many :answers, dependent: :destroy, foreign_key: :user_id
+
+  def author?(author)
+    author.user_id == id
+  end
 end
